14da100f03da498f70c5953e6880329f2a2c729b
Talal Balouch <tbalouch@gmail.com>
2019-03-10 00:53:29 -0500

    adding green colored echo text in install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 
 #Install all the requirements for the RapidRepoPull program and run
 
+green=`tput setaf 2`
+
 # Set virtual env with Python3
-echo "Setting up Python3 virtual env"
+echo "${green}Setting up Python3 virtual env"
 python3 -m venv venv
 
 # Activate virtual env
+echo "${green}Activating virtaul env"
 source venv/bin/activate
 
 # Upddate virtual pip version
+echo "${green}Upgrading virtual pip version"
 pip install --upgrade pip
 
 # Install requirements file
