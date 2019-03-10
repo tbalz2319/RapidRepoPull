fdd53c3a24d05bf4bde0e255c7572a64adb107a2
Talal Balouch <tbalouch@gmail.com>
2019-03-10 01:55:02 -0500

    adding support for green text in update_repos.sh

--- update_repos.sh
+++ update_repos.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # Mass update all indivudual repos
+
+# To help set echo output in green
+green=`tput setaf 2`
+
+# Save the name of all dirs to the dirs variable
 dirs=`ls -d */ | grep -v 'venv'`
 
 for dir in $dirs     # list directories in the current folder
 do
+    #Display dir name 
+    echo "${green}${dir}"
     #chain 3 commands in one line below
     cd ${dir} ; git pull ; cd ..
 done
