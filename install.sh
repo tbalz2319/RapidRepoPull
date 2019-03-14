95eebaf195a4629963a656b64b980ab9675cae69
Talal Balouch <tbalouch@gmail.com>
2019-03-14 11:44:11 -0400

    Update install.sh
    additional specific OS detection capability

--- install.sh
+++ install.sh
@@ -2,12 +2,26 @@
 
 # Install all the requirements for the RapidRepoPull program and run
 
-# Install required packages
-# The if statement below will only work on Linux (Ubuntu/Debian/Kali) based distros
-pkgs='python3-venv git'
-if ! dpkg -s $pkgs >/dev/null 2>&1; then
-  sudo apt-get install $pkgs
+# Install required packages based on running OS
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    # The if statement below will only work on Linux (Ubuntu/Debian/Kali) based distros
+    # It will not work with (Fedora/RHEL/Centos) distros
+    pkgs='python3-venv git'
+    if ! dpkg -s $pkgs >/dev/null 2>&1; then
+         sudo apt-get install $pkgs
+    fi
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
 fi
+
+
 
 green=`tput setaf 2`
 
