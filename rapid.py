3a5c7277e66cec15be91074fbf4b0fc412e2a753
Talal Balouch <tbalouch@gmail.com>
2019-03-22 07:51:49 -0400

    adding a few changes to support cli cmds to specify threads and files

--- rapid.py
+++ rapid.py
@@ -35,28 +35,35 @@ worker_data=["BloodHoundAD/BloodHound",
         "swisskyrepo/PayloadsAllTheThings",
         "robertdavidgraham/masscan",
         "bitsadmin/wesng",
-        "Yara-Rules/rules",
-        "codingo/Interlace"]
+        "needmorecowbell/Hamburglar"
+        "Yara-Rules/rules"]
+        #"codingo/Interlace"]
+
+#test initial list
 
 def threadCount(thread):
     # The command below kicks off thread dependent on how many CPU cores your system has available 
-    thread = multiprocessing.cpu_count() #Detect the available cores on system , similar to nproc
-    print("\nCreating %d threads...\n" % thread)
+    #thread = multiprocessing.cpu_count() #Detect the available cores on system , similar to nproc
+    # Reading CPU threads is no longer performed as this value is being privided by the user
+    # Convert user supplied thread value (String) to integer for consuption
+    cpus = int(thread)
+    print("\nCreating %d threads...\n" % cpus)
     #cpus = 10
     #print("\nPulling git repos with %d threads...\n" % cpus))
-    for i in range(thread):
+    for i in range(cpus):
       t = threading.Thread(target=worker)
       t.daemon = True
       t.start()
 
     q.join() # Blocks everything until all tasks in the queue have completed, then it print the messages below
     print("Program has successfully completed execution ...")
+    print('[%s]' % ', '.join(map(str, worker_data)))
     print(colored("Please check output ...", 'yellow'))
 
 # Load up a queue with the data from the worker_data list, this will handle locking
-q = queue.Queue()
-for git_repo in worker_data:
-    q.put(git_repo)
+# q = queue.Queue()
+# for git_repo in worker_data2:
+#     q.put(git_repo)
 
 # When acquired by a thread it locks other threads from printing
 lock = threading.Lock()
@@ -82,11 +89,7 @@ def subprocess_cmd(command):
     lock.release()
 
 # Worker function is defined below which will perform the work on the worker_data list
-def worker():
-    while not stop: 
-        item = q.get()
-        subprocess_cmd(["/usr/bin/git", "clone", "https://github.com/{}.git".format(item)])
-        q.task_done()
+
 
 def intro():
      # Display ASCII art from text file below
@@ -97,14 +100,76 @@ def intro():
 
 @click.command()
 @click.option('--verbose', '-v', multiple=True, is_flag=True, help="Will print verbose messages.")
-@click.option('--file', '-f', multiple=True,  default='' , help='a text file with a list of users favorite Github repos')
-@click.option('--thread', '-t', multiple=True, default='' , help='Number of CPU threads to use')
+# Interesting note below, the multiple option lets you changethe values of the option to a tuple if its true
+# If it is not true, then the value is a single value
+@click.option('--file', '-f', multiple=False,  default='' , help='a text file with a list of users favorite Github repos')
+@click.option('--thread', '-t', multiple=False, default='' , help='Number of CPU threads to use')
 def cli(verbose, file, thread):
+    worker_data2 = []
     if verbose:
         click.echo("We are in the verbose mode.")
-    click.echo("Aquired thread count value to use from user input...")
-    click.echo('The thread count to use is ... {0}'.format(thread))
-    click.echo('The filename which contains user defined repos is called {}'.format(file))
+    if thread:
+        click.echo("Aquired thread count value to use from user input...")
+        click.echo('The thread count to use is ... {0}'.format(thread))
+    if file:
+        click.echo('The filename which contains user defined repos is called {}'.format(file))
+        #Open user suppplied text file and append it to our existing worker_data list (array)
+        # f = open(file,'r')
+        # for line in f:
+        #     # Print lines in user supplied text file for testing
+        #     print(line.rstrip())
+        #     worker_data2.append(line)
+        #     worker_data2.save()
+        #     # print updated worker_data list to see its contents with added repos
+        #     print('[%s]' % ', '.join(map(str, worker_data2)))
+        #     # The print statement above proves that worker_data is now updated with the two additional repos
+        #test initial list
+        
+        with open(file) as repofile:
+            for line in repofile:
+                line = line.strip()
+                worker_data2.append(line)
+                print(worker_data2)
+
+        q = queue.Queue()
+        for git_repo in worker_data2:
+            q.put(git_repo)
+
+        def worker():
+          while not stop: 
+            item = q.get()
+            subprocess_cmd(["/usr/bin/git", "clone", "https://github.com/{}.git".format(item)])
+            q.task_done()
+
+        cpus = int(thread)
+        print("\nCreating %d threads...\n" % cpus)
+        #cpus = 10
+        #print("\nPulling git repos with %d threads...\n" % cpus))
+        for i in range(cpus):
+            t = threading.Thread(target=worker)
+            t.daemon = True
+            t.start()
+
+        q.join() # Blocks everything until all tasks in the queue have completed, then it print the messages below
+        print("Program has successfully completed execution ...")
+        print('[%s]' % ', '.join(map(str, worker_data2)))
+        print(colored("Please check output ...", 'yellow'))
+
+        
+                
+
+
+    
+    #Show intro ASCII Art (Step1)
+    intro()    
+    # The goal is to read the filename below if it were given 
+    
+    # The multiprocessing libary below is ready to be called at this point
+    # We want to make sure it takes in the "thread" parameter from the click cli function input from the user
+    
+
+    #threadCount(thread)
 
 if __name__ == "__main__":
+    # The cli function calls all other functions when it is executed 
     cli()
