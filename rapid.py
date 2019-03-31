41f3b63466c9867cf120f6d5a1c233ca9ef1ed0c
diego95root <minidiego@ono.com>
2019-03-31 01:18:28 +0100

    Removed repeated code, set thread default value to cores

--- rapid.py
+++ rapid.py
@@ -9,84 +9,6 @@ import threading
 import multiprocessing
 import giturlparse
 
-worker_data=["BloodHoundAD/BloodHound",
-        "GhostPack/Seatbelt",
-        "GhostPack/SharpUp",
-        "yeyintminthuhtut/Awesome-Red-Teaming",
-        "byt3bl33d3r/DeathStar",
-        "byt3bl33d3r/CrackMapExec",
-        "Cn33liz/p0wnedShell",
-        "EmpireProject/Empire",
-        "danielmiessler/SecLists",
-        "laramies/theHarvester",
-        "s0md3v/Photon",
-        "commixproject/commix",
-        "emtunc/SlackPirate",
-        "bwall/ExtractHosts",
-        "Grunny/zap-cli",
-        "tevora-threat/PowerView3-Aggressor",
-        "vysecurity/ANGRYPUPPY",
-        #"harleyQu1nn/AggressorScripts",
-        "bluscreenofjeff/AggressorScripts",
-        "pavanw3b/sh00t",
-        "evyatarmeged/Raccoon",
-        "1N3/IntruderPayloads",
-        "1N3/BlackWidow",
-        "trustedsec/ptf",
-        "swisskyrepo/PayloadsAllTheThings",
-        "robertdavidgraham/masscan",
-        "bitsadmin/wesng",
-        "Yara-Rules/rules",
-        "paranoidninja/CarbonCopy",
-        "Frint0/email-enum",
-        "needmorecowbell/Hamburglar",
-        "jondonas/linux-exploit-suggester-2",
-        "hlldz/Invoke-Phant0m",
-        "codingo/Interlace"]
-
-#test initial list
-q = queue.Queue()
-for git_repo in worker_data:
-    q.put(git_repo)
-
-def autoThread():
-    # The command below kicks off thread dependent on how many CPU cores your system has available 
-    thread = multiprocessing.cpu_count() #Detect the available cores on system , similar to nproc
-    # Convert user supplied thread value (String) to integer for consuption
-    cpus = int(thread)
-    print("\nCreating %d threads...\n" % cpus)
-    #print("\nPulling git repos with %d threads...\n" % cpus))
-    for i in range(cpus):
-      t = threading.Thread(target=worker)
-      t.daemon = True
-      t.start()
-
-    q.join() # Blocks everything until all tasks in the queue have completed, then it print the messages below
-    print("Program has successfully completed execution ...")
-    print('[%s]' % ', '.join(map(str, worker_data)))
-    print(colored("Please check output ...", 'yellow'))
-
-# The function below only kicks off if there is user specific threads provided to 
-# the program
-def threadCount(thread):
-    # The command below kicks off thread dependent on how many CPU cores your system has available 
-    #thread = multiprocessing.cpu_count() #Detect the available cores on system , similar to nproc
-    # Reading CPU threads is no longer performed as this value is being privided by the user
-    # Convert user supplied thread value (String) to integer for consuption
-    cpus = int(thread)
-    print("\nCreating %d threads...\n" % cpus)
-    #cpus = 10
-    #print("\nPulling git repos with %d threads...\n" % cpus))
-    for i in range(cpus):
-      t = threading.Thread(target=worker)
-      t.daemon = True
-      t.start()
-
-    q.join() # Blocks everything until all tasks in the queue have completed, then it print the messages below
-    print("Program has successfully completed execution ...")
-    print('[%s]' % ', '.join(map(str, worker_data)))
-    print(colored("Please check output ...", 'yellow'))
-
 # When acquired by a thread it locks other threads from printing
 lock = threading.Lock()
 stop = 0
@@ -110,14 +32,6 @@ def subprocess_cmd(command):
         print ("[***] Problem occurred while cloning {}: {}\n".format(name, error))
     lock.release()
 
-# Worker function is defined below which will perform the work on the worker_data list
-
-def worker():
-    while not stop: 
-        item = q.get()
-        subprocess_cmd(["/usr/bin/git", "clone", "https://github.com/{}.git".format(item)])
-        q.task_done()
-
 def intro():
      # Display ASCII art from text file below
     with open('ascii.txt', 'r') as f:
@@ -129,33 +43,39 @@ def intro():
 @click.option('--verbose', '-v', multiple=True, is_flag=True, help="Will print verbose messages.")
 # Interesting note below, the multiple option lets you changethe values of the option to a tuple if its true
 # If it is not true, then the value is a single value
-@click.option('--file', '-f', multiple=False,  default='' , help='Specify a text file with a list of user selected Github repos')
-@click.option('--thread', '-t', multiple=False, default='' , help='Specify the number of CPU threads to use')
+@click.option('--file', '-f', multiple=False,  default='default.txt' , help='Specify a text file with a list of user selected Github repos')
+@click.option('--thread', '-t', multiple=False, default=multiprocessing.cpu_count(), help='Specify the number of CPU threads to use')
+
 def cli(verbose, file, thread):
-    worker_data2 = []
+    worker_data = []
     if verbose:
         click.echo("We are in the verbose mode.")
     if thread:
-        click.echo("Aquired thread count value to use from user input...")
-        click.echo('The thread count to use is ... {0}'.format(thread))
+        click.echo("Aquired thread count value to use from user input")
+        if 0 >= thread:
+            click.echo("The thread count was 0 or negative, so the number of cores will be used".format(thread))
+            thread = multiprocessing.cpu_count()
+        click.echo('The thread count to use is: {0}'.format(thread))
     if file:
         click.echo('The filename which contains user defined repos is called {}'.format(file))
-        # Open user suppplied text file and append it to our existing worker_data list (array)
-        # Open text file provided by user which includes a list of user specified Github repos line by line
+        # Open either supplied text file or default file
+        # It includes a list of user specified Github repos line by line
         with open(file) as repofile:
             for line in repofile:
                 line = line.strip()
                 p = giturlparse.parse(line)
                 p_new = p.owner + '/' + p.repo
-                worker_data2.append(p_new)
-                print(worker_data2)
+                worker_data.append(p_new)
+                print(worker_data)
 
         q = queue.Queue()
-        for git_repo in worker_data2:
+        for git_repo in worker_data:
             q.put(git_repo)
 
-        def worker2():
-          while not stop: 
+        # Worker function is defined below which will perform the work on the worker_data list
+
+        def worker():
+          while not stop:
             item = q.get()
             subprocess_cmd(["/usr/bin/git", "clone", "https://github.com/{}.git".format(item)])
             q.task_done()
@@ -165,38 +85,20 @@ def cli(verbose, file, thread):
         #cpus = 10
         #print("\nPulling git repos with %d threads...\n" % cpus))
         for i in range(cpus):
-            t = threading.Thread(target=worker2)
+            t = threading.Thread(target=worker)
             t.daemon = True
             t.start()
 
         q.join() # Blocks everything until all tasks in the queue have completed, then it print the messages below
         print("Program has successfully completed execution ...")
-        print('[%s]' % ', '.join(map(str, worker_data2)))
+        print('[%s]' % ', '.join(map(str, worker_data)))
         print(colored("Please check output ...", 'yellow'))
-
-    #Show intro ASCII Art (Step1)
-    intro()    
-    # The goal is to read the filename below if it were given 
-    
-    # The multiprocessing libary below is ready to be called at this point
-    # We want to make sure it takes in the "thread" parameter from the click cli function input from the user
-
-    # This if/else seeks to check if the thread variable is empty
-    # If it is empty, it kicks the user to a seperate function that attempts to read the number
-    # of CPUs in the system and utilizing those for its execution
-    if not thread:
-        print("your_variable is empty")
-        autoThread()
-    else: 
-        try:
-            threadCount(thread)
-        except Exception as e:
-            print(str(e))
 
  # Initial main part of program below
  # The part below installs all built in repos consumed by the program
 
 if __name__ == "__main__":
-    # The cli function calls all other functions when it is executed 
+    #Show intro ASCII Art (Step1)
+    intro()
+    # The cli function calls all other functions when it is executed
     cli()
-    
