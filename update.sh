3d9acf82935276fd3be96b6fd8bea95ad7668650
Talal Balouch <tbalouch@gmail.com>
2019-04-11 01:22:50 -0400

    update to wording in echo statement in update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Update the repo before continuing
-echo "Checking for updates to RapidRepoPull Existing Repos..."
+echo "Checking for updates to existing repositories in the current RapidRepoPull dir..."
 git pull
