cd75217e2d40b1bd13ac41938e218180cb57a8d8
Talal Balouch <tbalouch@gmail.com>
2019-06-18 15:59:36 -0400

    update to boot.sh file which container runs to use 75 threads instead of 35

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # python3 -m venv venv 
 # source venv/bin/activate
-# Execute rapid.py in container
-time python rapid.py -v -t 35
+# Execute rapid.py in container with the default of 75 threads
+time python rapid.py -v -t 75
